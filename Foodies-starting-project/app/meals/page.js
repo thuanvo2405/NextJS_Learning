2a44b8f056c0d3a04561480819f7bc544d6e7bf9
Minh Thuận <vothuan2405003@gmail.com>
2025-08-01 16:46:35 +0700

    Video 107

--- Foodies-starting-project/app/meals/page.js
+++ Foodies-starting-project/app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from "next/link"
+import classes from "./page.module.css"
+import MealGrid from "@/components/meals/meals-grid"
+
 export default function MealPage() {
-    return <h1>This is meal page</h1>
+    return <>
+    <header className={classes.header}>
+        <h1>
+            Delicious meals, created
+            <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+            Choose your favorite recipe and cook it yourself.It is fun and easy!
+        </p>
+        <p className={classes.cta}>
+            <Link href="/meals/share">
+                Share Your Favorite Recipe
+            </Link>
+        </p>
+    </header>
+    <main className={classes.main}>
+        <MealGrid meals={[]}/>
+    </main>
+    </>
 }
---
+++ Foodies-starting-project/components/meals/meals-grid.js
@@ -0,0 +1,13 @@
+import MealItem from "./meal-item"
+import classes from "./meal-item.module.css"
+export default function MealGrid({ meals }) {
+    return (
+        <ul className={classes.meals}>
+            {meals.map((meal) => {
+                <li key={meal.id}>
+                    <MealItem {...meal} />
+                </li>
+            })}
+        </ul>
+    )
+}
